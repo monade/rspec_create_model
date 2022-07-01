95f21b9060c3e3fd37e03cba6bd6b029988d5fee
Stefano Lecchi <stefano@monade.io>
2022-07-01 12:34:47 +0200

    :new: Added specs.

--- spec/rspec_create_model/model_spec.rb
+++ spec/rspec_create_model/model_spec.rb
@@ -4,6 +4,37 @@ require "spec_helper"
 
 RSpec.describe "RspecCreateModel" do
   it "Matches the created record" do
-    expect { Mug.create!(color: "#000000", capacity: 1.50) }.to create_model Mug
+    expect { Author.create!(name: "Friedrich Nietzsche") }.to create_model Author
+  end
+
+  it "Matches the created record in callback" do
+    expect { Author.create!(name: "Friedrich Nietzsche") }
+      .to create_model(Author)
+      .and create_model(Book)
+  end
+
+  it "Created records are avaialable through instance variables" do
+    expect { Author.create!(name: "Friedrich Nietzsche") }
+      .to create_model(Author)
+
+    expect(@created_record).not_to be(nil)
+    expect(@created_records).not_to be(nil)
+
+    expect(@created_record).to be_a(Author)
+    expect(@created_records).to be_a(Array)
+
+    expect(@created_records.first).to be_a(Author)
+  end
+
+  it "Instance variables match the model we are examinating" do
+    expect { Author.create!(name: "Friedrich Nietzsche") }.to create_model(Author)
+
+    expect(@created_record).to be_a(Author)
+    expect(@created_records).to all(be_a(Author))
+
+    expect { Author.create!(name: "Friedrich Nietzsche") }.to create_model(Book)
+
+    expect(@created_record).to be_a(Book)
+    expect(@created_records).to all(be_a(Book))
   end
 end
--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -7,9 +7,20 @@ ActiveRecord::Base.establish_connection(
   database: ":memory:"
 )
 
-class Mug < ActiveRecord::Base
-  attribute :capacity, type: :Float
-  attribute :color, type: :String
+class Author < ActiveRecord::Base
+  attribute :name, type: :String
+  has_many :articles, dependent: :destroy
+
+  after_create :create_empty_book
+
+  def create_empty_book
+    Book.create!(author_id: id, title: "First Empty Book")
+  end
+end
+
+class Book < ActiveRecord::Base
+  belongs_to :author, optional: false
+  attribute :title, type: :String
 end
 
 module Schema
@@ -17,9 +28,15 @@ module Schema
     ActiveRecord::Migration.verbose = false
 
     ActiveRecord::Schema.define do
-      create_table :mugs, force: true do |t|
-        t.decimal :capacity
-        t.string :color
+      create_table :authors, force: true do |t|
+        t.decimal :name
+
+        t.timestamps null: false
+      end
+
+      create_table :books, force: true do |t|
+        t.belongs_to :author, foreign_key: true, on_delete: :cascade
+        t.string :title
 
         t.timestamps null: false
       end
